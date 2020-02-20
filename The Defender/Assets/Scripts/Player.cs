b4575e9157a1eb4665810667752d19384e3df8f5
Emmanuel Berchie <emmanuelberchie9@gmail.com>
2020-02-20 17:32:24 +0000

    Added enemy paths

--- The Defender/Assets/Scripts/Player.cs
+++ The Defender/Assets/Scripts/Player.cs
@@ -6,8 +6,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
-    [SerializeField] GameObject laserPrefab; 
-    
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+  
+
+    Coroutine firingCoroutine;
+
     float xMin;
     float xMax;
 
@@ -18,28 +23,47 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetUpMoveBoundaries(); 
+        SetUpMoveBoundaries();
     }
 
-    private void SetUpMoveBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding; 
-
-
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        move();
+        Move();
+        Fire();
     }
 
-    private void move()
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuosly());
+ 
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+
+    }
+
+    IEnumerator FireContinuosly()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(
+                  laserPrefab,
+                  transform.position,
+                  Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+
+
+    private void Move()
     {
         // determining where we want the ship to be and how to control it
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
@@ -50,6 +74,25 @@ public class Player : MonoBehaviour
         transform.position = new Vector2(newXPos, newYPos);
     
        
+
+    }
+
+
+   
+
+
+
+
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
 
     }
 }
